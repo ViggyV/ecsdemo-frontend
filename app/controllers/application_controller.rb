8a8730027a5d290849cfc67c64246e514b453d30
EC2 Default User <ec2-user@ip-172-31-57-36.us-west-2.compute.internal>
2021-08-18 15:47:38 +0000

    tried to change reset and update html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,13 @@ class ApplicationController < ActionController::Base
       #response from 
       req = Net::HTTP::Get.new(nodejs_uri.to_s)
       res = Net::HTTP.start(nodejs_uri.host, nodejs_uri.port, :use_ssl => nodejs_uri.scheme == 'https'){|http|
-        #http.read_timeout = 1000
-        #http.open_timeout = 1000
+        http.read_timeout = 1000
+        http.open_timeout = 1000
         http.request(req)
       }
       
-      @text = "noback"
-      #@file = res.bodt
+      @text = ""
+      @file = res.body
 
        if res.code == '200'
          @file = res.body
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
 
     rescue => e
       logger.error e.message
-      @text = "no bac"
+      @text = "error"
       @eee = e.message
     end
 
